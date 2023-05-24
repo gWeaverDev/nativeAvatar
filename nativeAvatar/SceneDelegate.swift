2ee6c687243f71c95ce6c56b05d34f62d243a513
gWeaverDev <124156429+gWeaverDev@users.noreply.github.com>
2023-05-24 13:35:35 +0300

    Update SceneDelegate.swift

--- nativeAvatar/SceneDelegate.swift
+++ nativeAvatar/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        let vc = TestVC()
+        let vc = ViewController()
         let nav = UINavigationController(rootViewController: vc)
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
